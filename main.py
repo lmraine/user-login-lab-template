16495f3da393f81e441336d16c3ca4c521310d37
lmraine <55120024+lmraine@users.noreply.github.com>
2024-01-15 17:11:22 -0600

    remove soln

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
+#REMOVE PASS AND FIX THE FUNCTION
 def create_login(user_string):
-    values = user_string.split()
-    login = ""
-    first_name_comp = values[0][0:1]
-    last_name_comp = values[1][:5]
-    digit_comp = values[2][-2:]
-    
-    login = last_name_comp + first_name_comp + digit_comp
-
-    return login
+    pass
 
 
 if __name__ == '__main__':
-    #remove pass and your code goes here
-    user_string = str(input())
-    login = create_login(user_string)
-    print(login)
+    #REMOVE PASS AND YOUR CODE GOES HERE
+    pass
     
